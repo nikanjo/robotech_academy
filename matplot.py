d60dd01ceb9b109f73489e9f326db99aab84af49
ebi mohamadi <nikanaghajonm@gmail.com>
2024-03-24 17:25:48 +0400

    matplotlib

---
+++ matplot.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#x = np.array([1,2])
+#y = np.array([10,15])
+
+#plt.plot(x,y, 'o')
+#plt.plot(x,y, 'o--m')
+
+
+#================
+
+#x = np.array([1,2,5,9])
+#y = np.array([10, 15, 19, 7])
+#plt.plot(x,y, 'v--b', linewidth = '5')
+#plt.show()
+
+#=================
+
+y1 = np.array([3, 8, 1, 10])
+y2 = np.array([6, 2, 7, 11])
+
+plt.plot(y1)
+plt.plot(y2)
+
+plt.show()
